93452b6b8994ad4accbd3d10620c5695f1a6c59e
Sai Rajesh <srajesh636@gmail.com>
2019-04-30 09:02:28 +0530

    Remove redux logger for production

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { reducer } from "../reducers";
-import logger from "redux-logger";
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer);
 
 export { store };
